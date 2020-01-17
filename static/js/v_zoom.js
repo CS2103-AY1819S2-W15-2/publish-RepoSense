a6e39229444d710e2fc4c86393fdeb4c7ceb1316
Deployment Bot (Travis) <deploy@travis-ci.org>
2020-01-17 10:52:04 +0000

    Rebuild pages at e64b16db89b791b28c48eb221107269868a0c8df

---
+++ static/js/v_zoom.js
@@ -0,0 +1,72 @@
+window.vZoom = {
+  props: ['info'],
+  template: window.$('v_zoom').innerHTML,
+  data() {
+    return {
+      filterTimeFrame: window.hashParams.timeframe,
+      showAllCommitMessageBody: true,
+      expandedCommitMessagesCount: this.totalCommitMessageBodyCount,
+    };
+  },
+  computed: {
+    filteredUser() {
+      const { user } = this.info;
+      const filteredUser = Object.assign({}, user);
+
+      const date = this.filterTimeFrame === 'week' ? 'endDate' : 'date';
+      filteredUser.commits = user.commits.filter(
+          (commit) => commit[date] >= this.info.sinceDate && commit[date] <= this.info.untilDate,
+      );
+
+      return filteredUser;
+    },
+    totalCommitMessageBodyCount() {
+      let nonEmptyCommitMessageCount = 0;
+      this.filteredUser.commits.forEach((commit) => {
+        commit.commitResults.forEach((commitResult) => {
+          if (commitResult.messageBody !== '' && commitResult.insertions > 0) {
+            nonEmptyCommitMessageCount += 1;
+          }
+        });
+      });
+
+      return nonEmptyCommitMessageCount;
+    },
+  },
+  methods: {
+    openSummary() {
+      this.$emit('view-summary', this.info.sinceDate, this.info.untilDate);
+    },
+
+    getSliceLink(slice) {
+      if (this.info.isMergeGroup) {
+        return `${window.getBaseLink(slice.repoId)}/commit/${slice.hash}`;
+      }
+      return `${window.getBaseLink(this.info.user.repoId)}/commit/${slice.hash}`;
+    },
+
+    toggleAllCommitMessagesBody(isActive) {
+      this.showAllCommitMessageBody = isActive;
+
+      const toRename = this.showAllCommitMessageBody ? 'commit-message active' : 'commit-message';
+
+      const commitMessageClasses = document.getElementsByClassName('commit-message');
+      Array.from(commitMessageClasses).forEach((commitMessageClass) => {
+        commitMessageClass.className = toRename;
+      });
+
+      this.expandedCommitMessagesCount = isActive ? this.totalCommitMessageBodyCount : 0;
+    },
+
+    updateExpandedCommitMessagesCount() {
+      this.expandedCommitMessagesCount = document.getElementsByClassName('commit-message active')
+          .length;
+    },
+  },
+  mounted() {
+    this.updateExpandedCommitMessagesCount();
+  },
+  components: {
+    v_ramp: window.vRamp,
+  },
+};
