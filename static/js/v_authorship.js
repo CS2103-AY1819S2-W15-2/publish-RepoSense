dec97fa0c62616c30cddea95f280853b8a206ddd
Deployment Bot (Travis) <deploy@travis-ci.org>
2020-04-20 13:22:27 +0000

    Rebuild pages at 9c9c9f40d4ed01b2c7f9f4e316e6a73eb11a27ec

--- static/js/v_authorship.js
+++ static/js/v_authorship.js
@@ -81,6 +81,10 @@ window.vAuthorship = {
         window.api.loadAuthorship(this.info.repo)
             .then((files) => this.processFiles(files));
       }
+
+      if (!this.info.fileTypeColors) {
+        this.$root.$emit('restoreFileTypeColors', this.info);
+      }
     },
 
     getRepoProps(repo) {
@@ -211,6 +215,7 @@ window.vAuthorship = {
 
           const segmentInfo = this.splitSegments(file.lines);
           out.segments = segmentInfo.segments;
+          out.blankLineCount = segmentInfo.blankLineCount;
           totalBlankLineCount += segmentInfo.blankLineCount;
 
           this.addBlankLineCount(file.fileType, segmentInfo.blankLineCount,
